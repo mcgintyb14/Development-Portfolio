49bd110765f84c9a9d8a374d220e1012af7adcb3
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-03-13 12:10:43 -0400

    updated aboutme content

--- src/components/Pages/aboutme.jsx
+++ src/components/Pages/aboutme.jsx
@@ -30,23 +30,28 @@ function AboutMe() {
             Macquarie Asset Management.
           </p>
           <p>
-            I have recently started a full time, 3-month coding Bootcamp through
-            UC Berkely to develop a suite of full stack development skills, and
+            I have recently graduated from a full time, 3-month coding Bootcamp through
+            UC Berkely which allowed me to develop a suite of full stack development skills, and
             this page will be used as a portfolio of my work as I continue to
-            develop and hone better skills.{" "}
+            develop and hone better skills. Please use this page to to browse some of my recent projects in the 'portfolio'
+            tab and feel free to reach out to me with either icon links below or the 'Contact Me' page!{" "}
           </p>
-          <h2 style={{ position: 'relative', left: '5%', marginBottom: '30px', marginTop: '40px'}}>
-            Work Bio
+          <h2 style={{ position: 'relative', left: '5%', marginBottom: '30px', marginTop: '40px', color: ' rgb(31, 150, 150)'}}>
+            Previous Employment
           </h2>
           <p>
-            Executive in the Non-Financial Risk Team within the Public
-            Investments business at Macquarie Asset Management. Focusing on
+            Before graduating from my Bootcamp, I was recently an Operational Risk Executive within the Non-Financial Risk Team within the Public
+            Investments business at Macquarie Asset Management, Focusing on
             strategic risk initiatives as well as assurance, incident and issue
             management, and risk framework uplift across the business.
             Previously a senior analyst in the Regulatory Affairs & Aggregate
-            Risk (RAAR) Team within RMG at Macquarie. Experienced in business
-            and risk advisory, including operational risk, financial risk, and
-            extensive RCSA experience. Graduated in May 2019 from Wharton’s
+            Risk (RAAR) Team within RMG at Macquarie.
+            </p>
+            <br></br>
+            <p>
+            I am experienced in business
+            as well as risk advisory; including operational risk, financial risk, and
+            extensive RCSA experience. I Graduated in May 2019 from Wharton’s
             Undergraduate School of Business with a concentration in Management.
             Recruited to play Varsity Football at the University of Pennsylvania
             and played for two years before suffering an injury and being
