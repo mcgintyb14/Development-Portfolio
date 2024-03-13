d1a3ff21ecf48610cbdbe8e00b04de1d3144e71d
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-03-13 13:18:07 -0400

    more edits made to the header list items

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 function Header() {
   // Define the background image style
   const headerStyle = {
-    backgroundImage: `url('/beach-quotes-1559667853.jpg')`, // Use a relative path
+    backgroundImage: `url('/photo-1589802829985-817e51171b92.avif')`, // Use a relative path
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    height: '250px', // Adjust the height as needed
+    height: '200px', // Adjust the height as needed
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -15,26 +15,33 @@ function Header() {
     color: 'white',
     textAlign: 'center',
     fontSize: '24px',
+    boxShadow: '0px 10px 8px rgba(0, 0, 0, 0.1)', // Add box shadow below the header
   };
 
   // Define the style for the list
   const listStyle = {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: 'white',
     listStyleType: 'none', // Remove bullets
     padding: 0, // Remove default padding
-    marginTop: '40%', // Remove default margin
-    height: '100%',
+    marginTop: '27%', // Remove default margin
+    position: 'relative',
   };
-
+  
   // Define the style for the list items
   const listItemStyle = {
-    margin: '0 10px', // Add spacing between list items
+    margin: '0 10px', // Add spacing between list items horizontally
+    marginRight: '10px', // Add 10px of space between list items horizontally
     backgroundColor: 'white',
     padding: '10px', // Adjust padding as needed
     borderRadius: '5px', // Add border radius for rounded corners
+    textDecoration: 'none', // Remove underlines from links
+    fontSize: '18px', // Set font size to 18px
+    width: '75%', // Reduce width by 25%
+    height: '50px', // Set fixed height for all list items
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center', // Center the text vertically
   };
 
   // Define the style for the active link
@@ -47,26 +54,26 @@ function Header() {
     <header style={headerStyle}>
       <nav>
         <ul style={listStyle}>
-          <li style={listItemStyle}>
-            <Link to="/resume" style={{ color: 'black' }} activeStyle={activeLinkStyle}>
+          <Link to="/resume" style={{ textDecoration: 'none' }}>
+            <li style={listItemStyle} activeStyle={activeLinkStyle}>
               Resume
-            </Link>
-          </li>
-          <li style={listItemStyle}>
-            <Link to="/contactme" style={{ color: 'black' }} activeStyle={activeLinkStyle}>
+            </li>
+          </Link>
+          <Link to="/contactme" style={{ textDecoration: 'none' }}>
+            <li style={listItemStyle} activeStyle={activeLinkStyle}>
               Contact Me
-            </Link>
-          </li>
-          <li style={listItemStyle}>
-            <Link to="/portfolio" style={{ color: 'black' }} activeStyle={activeLinkStyle}>
+            </li>
+          </Link>
+          <Link to="/portfolio" style={{ textDecoration: 'none' }}>
+            <li style={listItemStyle} activeStyle={activeLinkStyle}>
               Portfolio
-            </Link>
-          </li>
-          <li style={listItemStyle}>
-            <Link to="/aboutme" style={{ color: 'black' }} activeStyle={activeLinkStyle}>
+            </li>
+          </Link>
+          <Link to="/aboutme" style={{ textDecoration: 'none' }}>
+            <li style={listItemStyle} activeStyle={activeLinkStyle}>
               About Me
-            </Link>
-          </li>
+            </li>
+          </Link>
         </ul>
       </nav>
     </header>
--- src/components/Pages/aboutme.jsx
+++ src/components/Pages/aboutme.jsx
@@ -5,19 +5,36 @@ import headshot from "../../../public/headshot.jpg"; // Import the headshot imag
 function AboutMe() {
   return (
     <div
-      style={{ width: "80%", margin: "auto", position: "relative"}}
+      style={{
+        width: "80%",
+        margin: "auto",
+        position: "relative",
+        height: "100vh",
+      }}
       id="bio"
     >
-      <h1 style={{ position: 'relative', left: '20%', marginBottom: '30px',  marginTop: '40px'}}> About Me </h1>
+      <h1
+        style={{
+          position: "relative",
+          left: "20%",
+          marginBottom: "30px",
+          marginTop: "40px",
+        }}
+      >
+        {" "}
+        About Me{" "}
+      </h1>
       <div style={{ display: "flex", alignItems: "flex-start" }}>
         <img
           src={headshot}
-          alt="My Profile"
+          alt="My Headshot"
           style={{
             width: "250px",
             height: "250px",
-            borderRadius: "55%",
+            borderRadius: "10%",
             marginRight: "50px",
+            objectFit: "cover", // Ensure the image covers the entire container
+            objectPosition: "center", // Center the image within the container
           }}
         />
         <div>
@@ -30,37 +47,48 @@ function AboutMe() {
             Macquarie Asset Management.
           </p>
           <p>
-            I have recently graduated from a full time, 3-month coding Bootcamp through
-            UC Berkely which allowed me to develop a suite of full stack development skills, and
-            this page will be used as a portfolio of my work as I continue to
-            develop and hone better skills. Please use this page to to browse some of my recent projects in the 'portfolio'
-            tab and feel free to reach out to me with either icon links below or the 'Contact Me' page!{" "}
+            I have recently graduated from a full time, 3-month coding Bootcamp
+            through UC Berkely which allowed me to develop a suite of full stack
+            development skills, and this page will be used as a portfolio of my
+            work as I continue to develop and hone better skills. Please use
+            this page to to browse some of my recent projects in the 'portfolio'
+            tab and feel free to reach out to me with either icon links below or
+            the 'Contact Me' page!{" "}
           </p>
-          <h2 style={{ position: 'relative', left: '5%', marginBottom: '30px', marginTop: '40px', color: ' rgb(31, 150, 150)'}}>
+          <h2
+            style={{
+              position: "relative",
+              left: "5%",
+              marginBottom: "30px",
+              marginTop: "40px",
+              color: " rgb(31, 150, 150)",
+            }}
+          >
             Previous Employment
           </h2>
           <p>
-            Before graduating from my Bootcamp, I was recently an Operational Risk Executive within the Non-Financial Risk Team within the Public
+            Before graduating from my Bootcamp, I was recently an Operational
+            Risk Executive within the Non-Financial Risk Team within the Public
             Investments business at Macquarie Asset Management, Focusing on
             strategic risk initiatives as well as assurance, incident and issue
             management, and risk framework uplift across the business.
             Previously a senior analyst in the Regulatory Affairs & Aggregate
             Risk (RAAR) Team within RMG at Macquarie.
-            </p>
-            <br></br>
-            <p>
-            I am experienced in business
-            as well as risk advisory; including operational risk, financial risk, and
-            extensive RCSA experience. I Graduated in May 2019 from Wharton’s
-            Undergraduate School of Business with a concentration in Management.
-            Recruited to play Varsity Football at the University of Pennsylvania
-            and played for two years before suffering an injury and being
-            offered to stay on as a Student Coach for the remaining two years.
-            Professional experience leading risk management and marketing
-            efforts in large financial, pharmaceutical, and consulting firms.
-            Strengths include outstanding leadership skills, quickly developing
-            strong client and team relationships, excellent communication, and
-            driving superior results.
+          </p>
+          <br></br>
+          <p>
+            I am experienced in business as well as risk advisory; including
+            operational risk, financial risk, and extensive RCSA experience. I
+            Graduated in May 2019 from Wharton’s Undergraduate School of
+            Business with a concentration in Management. Recruited to play
+            Varsity Football at the University of Pennsylvania and played for
+            two years before suffering an injury and being offered to stay on as
+            a Student Coach for the remaining two years. Professional experience
+            leading risk management and marketing efforts in large financial,
+            pharmaceutical, and consulting firms. Strengths include outstanding
+            leadership skills, quickly developing strong client and team
+            relationships, excellent communication, and driving superior
+            results.
           </p>
         </div>
       </div>
